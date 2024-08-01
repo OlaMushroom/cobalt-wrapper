4190853be449af6dab7e1f1f4bcaad977d7bfc2b
Nyanto <ola.mushroom@gmail.com>
2024-08-01 21:38:46 +0700

    update

--- cli.js
+++ cli.js
@@ -2,50 +2,44 @@
 import { existsSync, mkdirSync, rmSync, writeFileSync } from 'node:fs'
 import { cac } from 'cac'
 import contentDisposition from 'content-disposition'
-import sendRequest from './lib/index.js'
+import cobalt from './lib/index.js'
 const dir = './downloaded/'
 const cli = cac('cobalt')
 cli.help()
 cli.option('-c, --clean', 'Clean the cache directory')
-function removeDir(dir) {
-  if (existsSync(dir)) {
-    try {
-      rmSync(dir, { recursive: true, force: true })
-      return true
-    } catch (e) {
-      throw Error('Error: ', { cause: e })
-    }
-  }
-  return false
-}
-async function download(url) {
-  try {
-    const res = await fetch(url)
-    if (!res.ok) throw Error(`HTTP Error: ${res.status} ${res.statusText}`)
-    const file = contentDisposition.parse(
-      res.headers.get('Content-Disposition')
-    ).parameters.filename
-    console.log(`Writing file...: ${file}`)
-    mkdirSync(dir, { recursive: true })
-    const path = dir + file
-    writeFileSync(path, new Uint8Array(await res.arrayBuffer()))
-    console.log(`File written successfully: ${path}`)
-  } catch (e) {
-    throw Error('Error: ', { cause: e })
-  }
-}
 cli
   .command('request <url>', 'Request download stream')
   .alias('r')
   .option('-d, --download', 'Download file')
   .action(async (url, options) => {
-    const req = await sendRequest({ url })
-    console.log(req)
-    if (options.download) download(req.url)
+    const data = await cobalt({ url })
+    console.log(data)
+    if (options.download) {
+      try {
+        const res = await fetch(data.url)
+        if (!res.ok) throw Error(`HTTP Error: ${res.status} ${res.statusText}`)
+        const file = contentDisposition.parse(
+          res.headers.get('Content-Disposition')
+        ).parameters.filename
+        console.log(`Writing file...: ${file}`)
+        mkdirSync(dir, { recursive: true })
+        const path = dir + file
+        writeFileSync(path, new Uint8Array(await res.arrayBuffer()))
+        console.log(`File written successfully: ${path}`)
+      } catch (e) {
+        throw Error('Error: ', { cause: e })
+      }
+    }
   })
 cli.parse()
 const opts = cli.options
 if (opts.clean) {
-  if (removeDir(dir)) console.log('Cleaned cache successfully.')
-  else console.log('No cache directory is found.')
+  if (existsSync(dir)) {
+    try {
+      rmSync(dir, { recursive: true, force: true })
+      console.log('Cleaned cache successfully.')
+    } catch (e) {
+      throw Error('Error: ', { cause: e })
+    }
+  } else console.log('No cache directory is found.')
 }
